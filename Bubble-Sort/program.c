3f4a766918ac1069d724392074d065b4665bfc5c
Shinjan-saha <shinjansaha02@gmail.com>
2024-02-07 21:14:47 +0530

    Bubble-sort-Updated

--- Bubble-Sort/program.c
+++ Bubble-Sort/program.c
@@ -1,29 +1,42 @@
- #include <stdio.h>
+#include <stdio.h>
 
- int main(){
-    
-    int i,j,swap,n;
-    int arr[100];
-    printf("Enter the number of elements you want in array: ");
-    scanf("%d",&n);
-    printf("Enter the Elements of the array");
-    for(i=0;i<n;i++){
-        scanf("%d",&arr[i]);
-    }
-    for (i=0; i<n-1;i++){
-        for(j=0;j<n-i-1;j++){
-            if(arr[i]>arr[j]){
-                swap=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=swap;
+void bubbleSort(int arr[], int n) {
+    int i, j;
+    for (i = 0; i < n-1; i++) {
+        // Last i elements are already in place
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                // Swap arr[j] and arr[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
     }
-printf("Sorted Array");
-for(i=0;i<n;i++){
-     printf("%d",arr[i]);
-
 }
-return 0;
 
- }
+int main() {
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    printf("Enter %d elements of the array:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    printf("Array before sorting: \n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+    
+    bubbleSort(arr, n);
+    
+    printf("Array after sorting: \n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+    
+    return 0;
+}
